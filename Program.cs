847397d76d34e97b9253bb46b0be810507c9d3bc
Natalia59 <maxi77@bk.ru>
2022-10-24 21:13:32 +0200

    Exercise-2

--- Program.cs
+++ Program.cs
@@ -1,58 +1,78 @@
-﻿//Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной 
-//диагонали (с индексами (0,0); (1;1) и т.д.)
-//Например, задан массив:
-//1472
-//5923
-//8424
-//Сумма элементов главной диагонали: 1+9+2 = 12
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
 
 
-Console.Write("ВВедите количество строк массива: ");
-int rows = int.Parse(Console.ReadLine() ?? "");
 
-Console.Write("ВВедите количество столбцов массива: ");
-int colums = int.Parse(Console.ReadLine() ?? "");
-
-int[,] array = GetArray(rows, colums, 0, 10);
-PrintArray(array);
-Console.WriteLine();
-int sum = SumArray(array);
-Console.WriteLine(sum);
-
-void PrintArray(int[,] inArray)
+static int GetPositiveNumberFromUser(string message, string errorMessage)
 {
-    for (int i = 0; i < inArray.GetLength(0); i++)
+    while (true)
     {
-        for (int j = 0; j < inArray.GetLength(1); j++)
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine() ?? "", out int userNumber) && userNumber > 0;
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+static int[,] GetArray(int row, int column, int minRandomValue, int maxRansomValue)
+{
+    int[,] randomArray = new int[row, column];
+
+    for (int rowIndex = 0; rowIndex < row; rowIndex++)
+    {
+        for (int columnIndex = 0; columnIndex < column; columnIndex++)
         {
-            Console.Write($"{inArray[i, j]} ");
+            randomArray[rowIndex, columnIndex] = new Random().Next(minRandomValue, maxRansomValue);
+        }
+    }
+    return randomArray;
+}
+
+static void PrintArray(int[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int column = 0; column < array.GetLength(1); column++)
+        {
+            Console.Write($"{array[row, column]} ");
         }
         Console.WriteLine();
     }
 }
-int[,] GetArray(int m, int n, int minValue, int maxValue)
+
+static bool CheckForUserIndexes(int[,] array, int row, int column)
 {
-    int[,] result = new int[m, n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            result[i, j] = new Random().Next(minValue, maxValue + 1);
-        }
-    }
-    return result;
+    if (row - 1 < array.GetLength(0) && column - 1 < array.GetLength(1))
+        return true;
+    return false;
 }
 
-int SumArray(int[,] array)
-{
-    int sum = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if(i == j)
-            sum += array[i,j];
-        }
-    }
-    return sum;
-}
+
+Console.Clear();
+int userRow = GetPositiveNumberFromUser("Enter number of rows are in your array : ", "Error. Try again.");
+
+int userColumn = GetPositiveNumberFromUser("EEnter number of columns are in your array : ", "Error. Try again.");
+
+int row = new Random().Next(1, 10);
+int column = new Random().Next(1, 10);
+int minRandomValue = new Random().Next(-9, 0);
+int maxRansomValue = new Random().Next(0, 10);
+int[,] array = GetArray(row, column, minRandomValue, maxRansomValue);
+
+Console.Clear();
+PrintArray(array);
+
+Console.WriteLine();
+
+bool checkResult = CheckForUserIndexes(array, userRow, userColumn);
+
+if (checkResult)
+    Console.WriteLine($"Element of array [{userRow},{userColumn}] = {array[userRow - 1, userColumn - 1]}.");
+else
+    Console.WriteLine($"[{userRow},{userColumn}] -> there is no element in the array.");
